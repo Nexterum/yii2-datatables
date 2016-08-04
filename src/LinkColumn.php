defa39926e008adb869bb1d6d1717c655c6d9e94
Nexterum <sergey@sychev.su>
2016-08-04 16:14:47 +0300

    Revert "multi links betta"
    This reverts commit bfe59bd3f9f1e78fcf55d095b986576f07d6a275.

--- src/LinkColumn.php
+++ src/LinkColumn.php
@@ -21,44 +21,25 @@ class LinkColumn extends \yii\base\Object
     public $render;
     public $searchable;
     public $orderable;
-    public $links = [];
 
     public function init()
     {
         $this->searchable = false;
         $this->orderable = false;
-        if(!empty($links)) {
-            foreach ($links as $link) {
-                if (empty($link->options['id'])) {
-                    $link->options['id'] = 'link';
-                }
-                if (!isset($this->render)) {
-                    $this->render = new JsExpression('function render( data, type, row, meta ){
-            var p = ' . Json::encode($this->queryParams) . ';
-            var q = {};for (var i = 0; i < p.length; i++) {q[p[i]] = row[p[i]];}
-            var link = jQuery(\'' . Html::a($link->label, $link->url, $link->options) . '\');
-            var paramPrefix = ((link.attr("href").indexOf("?") < 0) ? "?" : "&");
-            link.attr("id", link.attr("id") + meta.row);link.attr("href", link.attr("href") + paramPrefix + jQuery.param(q));
-            return link.get()[0].outerHTML;}');
-                }
-            }
-        } else {
-            if (empty($this->options['id'])) {
-                $this->options['id'] = 'link';
-            }
 
-            if (!isset($this->render)) {
-                $this->render = new JsExpression('function render( data, type, row, meta ){
+        if (empty($this->options['id'])) {
+            $this->options['id'] = 'link';
+        }
+
+        if (!isset($this->render)) {
+            $this->render = new JsExpression('function render( data, type, row, meta ){
             var p = ' . Json::encode($this->queryParams) . ';
             var q = {};for (var i = 0; i < p.length; i++) {q[p[i]] = row[p[i]];}
             var link = jQuery(\'' . Html::a($this->label, $this->url, $this->options) . '\');
             var paramPrefix = ((link.attr("href").indexOf("?") < 0) ? "?" : "&");
             link.attr("id", link.attr("id") + meta.row);link.attr("href", link.attr("href") + paramPrefix + jQuery.param(q));
             return link.get()[0].outerHTML;}');
-            }
         }
-
-
     }
 
 
